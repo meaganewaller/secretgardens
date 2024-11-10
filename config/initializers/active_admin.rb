c7f81d11f6d51a91cc71f44378cf84a341d2d25d
Meagan Waller <meagan@meaganwaller.com>
2024-11-10 14:11:40 -0500

    Adding static text to locales

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Secretgardens"
+  config.site_title = "Secret Gardens"
 
   # == Load Paths
   #
